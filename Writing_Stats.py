282d5e0e2ec02179ce3a1f365f17b3453154cbac
thpossidente <thpossidente@vassar.edu>
2017-07-31 22:05:13 -0400

    tkinter stuff

--- Writing_Stats.py
+++ Writing_Stats.py
@@ -5,6 +5,7 @@
 #nltk.download()  #only once
 
 from tkinter import *
+from tkinter import ttk
 import nltk
 import collections
 import re
@@ -49,7 +50,6 @@ def get_docx_text(path):
     return '\n\n'.join(paragraphs)
     
     
-
 
 
 
@@ -391,11 +391,32 @@ def writing_stats(text):
        )
 
 
+     ### Tkinter UI ###
+  
+root = Tk()
+root.title("Writing Statistics")
 
-writing_stats(get_docx_text("C:\\Users\\Tom\\Downloads\\1082CreationMyth.docx")) 
+mainframe = ttk.Frame(root, padding='20 20 20 40')
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+text = StringVar()
+
+filepath = ttk.Entry(mainframe, width=25, textvariable=text)
+filepath.grid(column=2, row=1, sticky=W)
+ttk.Label(mainframe, text='Enter File Path to Word document:  ').grid(column=1, row=1, sticky=(W))
+
+button = ttk.Button(mainframe, text='Get Writing Statistics', command=writing_stats)
+button.place(relx=0.5, rely=2, anchor=CENTER)
+
+root.mainloop()
+
+
+#writing_stats(get_docx_text("C:\\Users\\Tom\\Downloads\\1082CreationMyth.docx")) 
 #writing_stats(text)   Use if no word doc input and you want to be prompted to enter text (comment out line above)
 
-
+writing_stats(str(text))
 
         ##Notes##
 
